7b54e250a95fa054f58cf964692ad5eee8d8be15
Pavol Kuchar <pavol.kuchar@outlook.com>
2023-10-20 10:33:18 -0400

    added explanations

--- sync.py
+++ sync.py
@@ -2,13 +2,19 @@ import requests
 
 base_url = "http://httpbin.org/"
 
+# defining function that takes number of seconds
 def get_delay(seconds):
+
+    # building an endpoint with that number of seconds included
     endpoint = f'/delay/{seconds}'
 
     print(f'Getting with {seconds} delay...')
 
-    resp = requests.get(base_url+endpoint)
-    data = resp.json()
+    # calling that url here:
+
+    response = requests.get(base_url+endpoint)
+    data = response.json()
+    # when it returns it prints out the data
     print(data)
 
 get_delay(5)
