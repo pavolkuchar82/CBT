f8ea5b49cbb857ea880b6e0da848e37fb34e6ace
Pavol Kuchar <pavol.kuchar@outlook.com>
2023-10-20 11:36:33 -0400

    added respcode.py

---
+++ respcodes.py
@@ -0,0 +1,14 @@
+import requests
+
+base_url = "https://api.discogs.com/"
+
+def get_releases(release_id):
+
+    endpoint = f'/releases/{release_id}'
+
+    print(f'Getting release n: {release_id}')
+
+    response = requests.get(base_url+endpoint)
+    print(response)
+
+get_releases(28571377)
